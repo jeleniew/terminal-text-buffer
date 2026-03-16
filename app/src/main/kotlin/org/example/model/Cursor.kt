0f3b282aa625a59e0e44d3a3794aa3b0f02fdadd
Wiktoria Jeleniewicz <jeleniewiczwiktoria@gmail.com>
2026-03-16 18:31:44 +0100

    Cursor get/set and move

--- app/src/main/kotlin/org/example/model/Cursor.kt
+++ app/src/main/kotlin/org/example/model/Cursor.kt
@@ -3,4 +3,29 @@ package org.example.model
 class Cursor(
     var column: Int = 0,
     var row: Int = 0,
-)
+) {
+    fun getPostion(): Pair<Int, Int> {
+        return Pair(column, row)
+    }
+
+    fun setPosition(column: Int, row: Int) {
+        this.column = column
+        this.row = row
+    }
+
+    fun moveUp() {
+        row--
+    }
+
+    fun moveDown() {
+        row++
+    }
+
+    fun moveLeft() {
+        column--
+    }
+
+    fun moveRight() {
+        column++
+    }
+}
